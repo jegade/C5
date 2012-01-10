a4e1cc45e662733f95130a068540e0efece02963
Jens Gassmann <jens.gassmann@atomix.de>
2012-01-10 23:16:40 +0100

    Prototype C5

--- lib/C5/Repository.pm
+++ lib/C5/Repository.pm
@@ -11,36 +11,33 @@ sub new {
     my ( $self, $options ) = @_;
 
     my $base = bless {}, $self;
-    
-    $base->{remote}  = C5::Repository::Remote->new($base, $options );
-    $base->{storage} = C5::Repository::Storage->new($base, $options );
-    $base->{manage}  = C5::Repository::Manage->new($base, $options );
 
-    $base->{ug}      = Data::UUID::MT->new;
+    $base->{remote} = C5::Repository::Remote->new( $base, $options );
+    $base->{storage} = C5::Repository::Storage->new( $base, $options );
+    $base->{manage} = C5::Repository::Manage->new( $base, $options );
+
+    $base->{ug} = Data::UUID::MT->new;
 
     return $base;
 }
-
 
 =head2 create
 
 
 =cut
 
-
 sub create {
 
     my ( $self, $meta, $payload ) = @_;
 
-    $meta->{uuid}    ||= $self->{ug}->create_string;
-    $meta->{type}    ||= "base";
-    $meta->{create}  ||= time();
+    $meta->{uuid}   ||= $self->{ug}->create_string;
+    $meta->{type}   ||= "base";
+    $meta->{create} ||= time();
 
-    my $obj = C5::Repository::Object->new( $self, $meta, $payload  );
+    my $obj = C5::Repository::Object->new( $self, $meta, $payload );
 
-    return ;
+    return;
 }
-
 
 =head2 get
 
@@ -52,15 +49,15 @@ sub get {
 
     my ( $self, $uuid ) = @_;
 
-    my ( $meta, $payload ) = $self->base->storage->get( $uuid ) ;
-    return self->create( $meta, $payload ) ;
+    my ( $meta, $payload ) = $self->base->storage->get($uuid);
+    return self->create( $meta, $payload );
 }
-
 
 sub get_raw {
 
     my ( $self, $uuid ) = @_;
 
+    my ( $meta, $payload ) = $self->base->storage->get($uuid);
 }
 
 =head2 query
@@ -73,22 +70,20 @@ sub query {
 
     my ( $self, $query ) = @_;
 
-    return $self->storage->query( $query ) ;
+    return $self->storage->query($query);
 }
 
 sub query_raw {
 
     my ( $self, $query ) = @_;
 
-    return $self->storage->query_raw( $query ) ;
+    return $self->storage->query($query);
 }
-
 
 sub storage {
 
     shift->{storage};
 }
-
 
 sub remote {
 
@@ -101,16 +96,12 @@ sub manage {
     shift->{manage};
 }
 
-
 1;
-
-
 
 =head1 C5::Repository::Storage
 
 
 =cut
-
 
 package C5::Repository::Storage;
 
@@ -120,16 +111,81 @@ package C5::Repository::Storage;
 
 =cut
 
-sub new  {
+sub new {
 
-    my ( $package, $options ) = @_;
+    my ( $package, $base, $options ) = @_;
+    $options->{base} = $base;
     return bless $options, $package;
 }
 
+=head2 db
+
+=cut
+
+sub db {
+
+    my $self = shift;
+    use MongoDB::OID;
+    return $self->{db} ||= MongoDB::Connection->new( $self->{options}{db} );
+}
+
+=head2 base
+
+=cut
+
 sub base {
 
-    my ( $self ) = @_;
+    my ($self) = @_;
     return $self->{base};
+}
+
+=head2 get
+
+=cut
+
+sub get {
+
+    my ( $self, $uuid ) = @_;
+
+    my $raw = $self->db->find_one( { uuid => $uuid } );
+
+    if ($raw) {
+
+        return ( $raw->{meta}, $raw->{payload} );
+
+    } else {
+
+        return;
+    }
+
+}
+
+sub create {
+
+    my ( $self, $meta, $payload ) = @_;
+
+    return $self->db->insert();
+
+}
+
+=head2 list_uuids
+
+=cut
+
+sub list_uuids {
+
+    my ($self) = @_;
+    return [ map { [ $_->{uuid}, $_->{created} ] } $self->db->find( {}, { uuid => 1, created => 1 } )->all ];
+}
+
+=head2 list_uuids_older
+
+=cut
+
+sub list_uuids_older {
+
+    my ( $self, $older ) = @_;
+    return [ map { [ $_->{uuid}, $_->{created} ] } $self->db->find( { created => { '$lt' => $older } }, { uuid => 1, created => 1 } )->all ];
 }
 
 1;
@@ -139,7 +195,6 @@ sub base {
 
 =cut
 
-
 package C5::Repository::Object;
 
 =head2 new
@@ -148,7 +203,7 @@ package C5::Repository::Object;
 
 =cut
 
-sub new  {
+sub new {
 
     my ( $package, $ref, $meta, $payload ) = @_;
     return bless { %$ref, meta => $meta, payload => $payload }, $package;
@@ -156,7 +211,7 @@ sub new  {
 
 sub base {
 
-    my ( $self ) = @_;
+    my ($self) = @_;
     return $self->{base};
 
 }
@@ -177,8 +232,7 @@ sub update {
 
 sub propagate {
 
-    my ( $self ) = @_;
-
+    my ($self) = @_;
 
 }
 
@@ -188,14 +242,11 @@ sub propagate {
 
 sub distribute {
 
-    my ( $self ) = @_;
-
+    my ($self) = @_;
 
 }
 
-
 1;
-
 
 =head1 C5::Repository::Manage
 
@@ -203,6 +254,29 @@ sub distribute {
 =cut
 
 package C5::Repository::Manage;
+
+=head2 new 
+
+=cut
+
+sub new {
+
+    my ( $self, $base, $options ) = @_;
+
+    $options->{base} = $base;
+
+    return bless $options, $self;
+}
+
+=head2 base
+
+=cut
+
+sub base {
+
+    my ($self) = @_;
+    return $self->{base};
+}
 
 =head2 rsync
 
@@ -214,15 +288,15 @@ sub rsync {
 
     my ( $self, $remote ) = @_;
 
-    # Get every local object  [ [ uuid, epoch ], …  ] 
+    # Get every local object  [ [ uuid, epoch ], …  ]
     my $list = $self->base->storage->list_uuids;
 
     # Query remote for missing objects
-    my $missings = $self->remote->check_for_updates( $list);
+    my $missings = $self->remote->check_for_updates($list);
 
-    foreach my $uuid ( @$missings ) {
-        my $obj = $self->base->get( $uuid );
-        $self->base->remote->update($remote, $obj) ;
+    foreach my $uuid (@$missings) {
+        my $obj = $self->base->get($uuid);
+        $self->base->remote->update( $remote, $obj );
     }
 }
 
@@ -236,15 +310,14 @@ sub purge {
 
     my ( $self, $older ) = @_;
 
-    my $list = $self->base->storage->list_uuids_older( $older ) ;
+    my $list = $self->base->storage->list_uuids_older($older);
 
-    foreach my $uuid ( @$list ) {
-        $self->base->storage->drop( $uuid ) ;
+    foreach my $uuid (@$list) {
+        $self->base->storage->drop($uuid);
     }
 
     return $list;
 }
-
 
 1;
 
@@ -256,16 +329,17 @@ sub purge {
 
 package C5::Repository::Remote;
 
-
 =head2 new
 
 =cut
 
 sub new {
-    
-    my ( $self, $config ) = @_;
-    
-    return bless {}, $self;
+
+    my ( $self, $base, $config ) = @_;
+
+    $config->{base} = $base;
+
+    return bless $config, $self;
 }
 
 =head2 check_for_updates
@@ -287,7 +361,6 @@ sub update {
 
     foreach my $remote ( $self->remotes ) {
 
-
     }
 
 }
@@ -301,16 +374,16 @@ sub update {
 sub retrieve {
 
     my ( $self, $cmd, $string ) = @_;
-    
+
 }
 
 =head2 remotes
 
 =cut
 
-sub remotes  {
+sub remotes {
 
-    return {shift->{remotes}};
+    return { shift->{remotes} };
 }
 
 1;
